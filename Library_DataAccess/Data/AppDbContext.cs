11838d0286d1104f59c357f040d60f03b70d6c89
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-02-29 16:17:15 -0300

    Add DB Connection and Created User Tables with Identity and Custom Identity User.

---
+++ Library_DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Library_DataAccess.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_DataAccess.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
